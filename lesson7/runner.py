3ee908f71a89b93ee4ca6cd4265656671eba25a6
Domasniy <domasniy@mail.ru>
2020-06-26 04:00:21 +0300

    Add files via upload

---
+++ lesson7/runner.py
@@ -0,0 +1,12 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+
+from leroy.spiders.leroyru import LeroyruSpider
+from leroy import settings
+
+if __name__ == '__main__':
+    crawler_settings = Settings()
+    crawler_settings.setmodule(settings)
+    process = CrawlerProcess(settings=crawler_settings)
+    process.crawl(LeroyruSpider, search=['плинтус'])  
+    process.start()
